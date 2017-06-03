7761b7ae8ab85c3ae2a8e1855e2586a7298e64db
Johyn Papin <johynpapin@kiwiasso.org>
2017-06-03 16:20:19 +0200

    Added subscription

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -6,6 +6,9 @@ import '../../ui/pages/home-page';
 
 FlowRouter.route('/', {
 	name: 'homePage',
+	subscriptions: function(params, queryParams) {
+		this.register('my', Meteor.subscribe('blogPost', params.postId));
+	},
 	action: function () {
 		BlazeLayout.render('mainLayout', { content: 'homePage' });
 	}
--- imports/ui/pages/home-page.js
+++ imports/ui/pages/home-page.js
@@ -1,8 +1,15 @@
 import { Template } from 'meteor/templating';
+import { Contents } from '../../api/contents/contents';
 
 import '../components/content-component';
 
 import './home-page.html';
+
+Template.homePage.onCreated(function () {
+	this.autorun(() => {
+		this.subscribe('contents', [], [], 10);
+	});
+});
 
 Template.homePage.onRendered(() => {
 	const target = $('#right-sidebar').find('.pushpin');
