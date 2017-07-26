f5feda14c7227acbaddc6a8a413c7fd2d9e69f97
Johyn Papin <johynpapin@kiwiasso.org>
2017-07-26 23:04:28 +0200

    Cleaning

--- imports/ui/pages/rtfm-page.js
+++ imports/ui/pages/rtfm-page.js
@@ -1,3 +1,19 @@
 import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
 
 import './rtfm-page.html';
+
+Template.rtfmPage.onCreated(function rtfmPageOnCreated () {
+	this.answer = new ReactiveVar(0);
+	const answerInterval = Meteor.setInterval(() => {
+		this.answer.set(this.answer.get() + 1);
+		if (this.answer.get() === 42) {
+			Meteor.clearInterval(answerInterval);
+		}
+	}, 500);
+});
+Template.rtfmPage.helpers({
+	answer() {
+		return Template.instance().answer.get();
+	}
+});
