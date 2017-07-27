5eae5b78b4b241ac67b0e80c5c9121d2f3b27dd6
Johyn Papin <johynpapin@kiwiasso.org>
2017-07-27 19:05:18 +0200

    Renamed home-page

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,16 +2,16 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 import '../../ui/layouts/main-layout';
-import '../../ui/pages/home-page';
+import '../../ui/pages/feed-page';
 
 import '../../ui/layouts/admin-layout';
 import '../../ui/pages/admin-home-page';
 import '../../ui/pages/rtfm-page';
 
 FlowRouter.route('/', {
-	name: 'homePage',
+	name: 'feedPage',
 	action() {
-		BlazeLayout.render('mainLayout', { content: 'homePage' });
+		BlazeLayout.render('mainLayout', { content: 'feedPage' });
 	}
 });
 
