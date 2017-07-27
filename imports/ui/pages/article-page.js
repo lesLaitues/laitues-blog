6b5bba25b690ecabf5bc504c35b35d1a13a687e4
Johyn Papin <johynpapin@kiwiasso.org>
2017-07-27 19:20:27 +0200

    Debug

--- imports/ui/pages/article-page.js
+++ imports/ui/pages/article-page.js
@@ -1,22 +1,31 @@
 import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/kadirahq:flow-router';
 
 import '../components/tag-component';
 import './article-page.html';
 
 import { Tags } from '../../api/tags/tags';
 import { Comments } from '../../api/comments/comments';
+import { Articles } from '../../api/articles/articles';
 
 Template.articlePage.helpers({
+	article() {
+		return Articles.findOne(FlowRouter.getParam('articleId'));
+	},
 	tag(tagId) {
 		return Tags.findOne(tagId);
 	},
 	commentsCount(articleId) {
-		return Comments.find({ article: articleId }).count();
+		return Comments.find({ article: FlowRouter.getParam('articleId') }).count();
 	},
 	topLevelComments(articleId) {
 		return Comments.find({
-			article: articleId,
-			$not: { $exists: following }
+			article: FlowRouter.getParam('articleId'),
+			$not: {
+				$exists: {
+					following: 1
+				}
+			}
 		});
 	}
 });
